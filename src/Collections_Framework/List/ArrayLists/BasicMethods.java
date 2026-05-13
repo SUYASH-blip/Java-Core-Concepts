b45721c8f3487ef0a45b45c2ddfe90dfcb139049
suyash giri <suyashgiri938@gmail.com>
2026-05-13 19:40:32 +0530

    Basic Methods of ArrayLists added

---
+++ src/Collections_Framework/List/ArrayLists/BasicMethods.java
@@ -0,0 +1,31 @@
+package Collections_Framework.List.ArrayLists;
+import java.util.ArrayList;
+import java.util.List;
+public class BasicMethods {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("Suyash");
+        list.add("Tushar");
+        list.add("Vinay");
+        list.add("Tanish");
+        list.add("Vinayak");
+        list.add(5,"Swati");
+        list.get(0);
+
+        boolean y = list.contains("Swati");
+        System.out.println(y);
+        list.remove(5);
+        boolean z = list.contains("Swati");
+        System.out.println(z);
+
+        //For each Printing
+        for(String x:list){
+            System.out.println(x);
+        }
+
+        //Direct Printing Because of toString()
+        System.out.println(list);
+
+
+    }
+}
