d51825cd21aac4446662eb9cb94b1574e302942a
suyash giri <suyashgiri938@gmail.com>
2026-05-21 23:12:19 +0530

    Added

---
+++ src/Collections_Framework/List/ArrayLists/NewClass.java
@@ -0,0 +1,23 @@
+package Collections_Framework.List.ArrayLists;
+import java.util.ArrayList;
+
+import java.util.*;
+public class NewClass {
+
+
+        public static void main(String[] args) {
+            ArrayList<Integer> l1 = new ArrayList<>();
+            ArrayList<Integer> l2 = new ArrayList<>();
+            l1.add(1);
+            l1.add(2);
+            l1.add(3);
+            l2.add(4);
+            l2.add(5);
+            l2.add(6);
+            l1.addAll(l2);
+            System.out.println(l1);
+
+        }
+    }
+
+
