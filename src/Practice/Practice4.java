40f522b0e0a1b34de4a42071f641a176f5bb9836
suyash giri <suyashgiri938@gmail.com>
2026-06-14 18:51:26 +0530

    Added

---
+++ src/Practice/Practice4.java
@@ -0,0 +1,29 @@
+package Practice;
+import java.util.*;
+
+public class Practice4 {
+    public static void main(String[] args) {
+        List<Students> s = new LinkedList<>();
+        s.add(new Students(95,"Suyash",21));
+        s.add(new Students(94,"Jay",22));
+        s.add(new Students(93,"Vijay",20));
+
+        Comparator<Students> work = (s1,s2) -> s1.marks-s2.marks;
+        Collections.sort(s,work);
+
+        for(Students st:s){
+            System.out.println(st.marks+" , "+st.name+" , "+st.roll);
+        }
+    }
+}
+class Students{
+    int marks;
+    String name;
+    int roll;
+
+    public Students(int marks, String name, int roll) {
+        this.marks = marks;
+        this.name = name;
+        this.roll = roll;
+    }
+}
