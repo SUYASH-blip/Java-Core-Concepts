bfc082e26f5e8b707b05ee560b0f7577a135713f
suyash giri <suyashgiri938@gmail.com>
2026-06-16 00:26:43 +0530

    Terminal Functions Added

--- src/STREAMS/Terminal_functions.java
+++ src/STREAMS/Terminal_functions.java
@@ -2,6 +2,8 @@ package STREAMS;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class Terminal_functions {
     public static void main(String[] args) {
@@ -10,6 +12,76 @@ public class Terminal_functions {
         //1. Collecting Results:-
              //a.  toList:- return immutable list
              //b.  collect:- one of the most powerful methods in whole streams topic
-             //    because there is a separate interface "Collector" which provides multiple methods to
+             //    because there is a separate interface "Collector" which provides multiple methods to collect a
+             //    stream in different ways.
+
+        List<Integer> list2 = list.stream()
+                .map(x -> x + 1)
+                .toList();
+
+
+        List<Integer> list3 = list.stream()
+                .map(x -> x + 1)
+                .collect(Collectors.toList());  //mutable list, Collectors is a implementation class of the collector
+                                               // interface
+
+        list3.add(4);
+        System.out.println(list3);
+
+
+
+        //2. Reducing Streams:- combines streams all elements into a single value
+        //There are two overloaded methods of the reduce()
+
+        //1.Method - 1 :-
+
+        Optional<Integer> sum = list.stream()
+                .reduce((a,b) -> a + b);
+        System.out.println(sum.get());
+
+        //Method-2 :-
+
+        int sum2 = list.stream()
+                .reduce(0,(a,b) -> a + b);
+        System.out.println(sum2);
+
+
+        //count() --> return number of elements which passed all the filters
+
+        long ans = list.stream()
+                .filter(x -> x >10)
+                .map(x -> x * 9)
+                .count();             //returns a long value
+        System.out.println(ans);
+
+       //FindFirst():- Returns first element to pass all the filters , it performs short circuiting becuase after
+        // returning a value it closes the stream
+
+       Optional<Integer> o = list.stream()
+                .filter(x -> x >10)
+                .map(x -> x * 9)
+                .findFirst();
+
+        System.out.println(o.get());
+
+        //Similarly FindAny mostly used for parallel streams
+
+        //AnyMatch():- returns a boolean value
+
+        boolean bool = list.stream()
+                .filter(x -> x >10)
+                .map(x -> x * 9)
+                .anyMatch(x -> x -2 == 0);
+
+        System.out.println(bool);
+
+        //Similarly allMatch() and noneMatch()
+
+
+
+
+
+
+
     }
 }
