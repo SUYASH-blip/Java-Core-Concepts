180ff162e929ada97e194f739b37bae76ec41127
suyash giri <suyashgiri938@gmail.com>
2026-06-21 19:49:46 +0530

    Optionals Class Added

---
+++ src/Optional/BasicProgram.java
@@ -0,0 +1,29 @@
+package Optional;
+import java.util.Optional;
+public class BasicProgram {
+
+
+
+        public static void main(String[] args) {
+            Optional<String> name = getName();
+
+            // if(name.isPresent()) {
+            //     System.out.println(name.get());
+            // }
+
+            //name.ifPresent(System.out::println);
+
+            // System.out.println(name.orElse("Unknown"));
+
+            // System.out.println(name.orElseGet(() -> "Unknown"));
+
+            //System.out.println(name.orElseThrow());
+
+            name.ifPresentOrElse(System.out::println, () -> System.out.println("Unknown"));
+        }
+
+        public static Optional<String> getName() {
+            return Optional.ofNullable("Aditya");
+        }
+    }
+
