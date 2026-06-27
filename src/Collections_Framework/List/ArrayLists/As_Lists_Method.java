180be65363f5ddbf9982a4a5a9a3e7c3bba1132d
suyash giri <suyashgiri938@gmail.com>
2026-06-27 21:31:02 +0530

    Added

--- src/Collections_Framework/List/ArrayLists/As_Lists_Method.java
+++ src/Collections_Framework/List/ArrayLists/As_Lists_Method.java
@@ -5,7 +5,7 @@ public class As_Lists_Method {
         List<String> List1 = Arrays.asList("Suyash","Kanha","govind");//To make it more Flexible
         //as lists when stored like this is even when stored in a lists but it is restricted.
         List1.remove("Suyash");//will throw Runtime error
-       //  List1.add("Jay");//Same error
+         List1.add("Jay");//Same error
          List1.set(0,"Radhe");
          System.out.println(List1);
 
