5c058fe43dced956be5758d81b1d5cd4e92e16a8
suyash giri <suyashgiri938@gmail.com>
2026-06-30 23:13:04 +0530

    Added

--- src/Collections_Framework/List/ArrayLists/BinarySearch.java
+++ src/Collections_Framework/List/ArrayLists/BinarySearch.java
@@ -3,7 +3,7 @@ package Collections_Framework.List.ArrayLists;
 public class BinarySearch {
 
         public static void main(String[] args) {
-            int[] arr = {10, 20, 30, 40, 50, 60, 70};
+           int[] arr = {10, 20, 30, 40, 50, 60, 70};
             int key = 50;
 
             int low = 0;
@@ -26,7 +26,7 @@ public class BinarySearch {
             if (position != -1) {
                 System.out.println("Element found at index: " + position);
             } else {
-                System.out.println("Element not found");
+                //System.out.println("Element not found");
             }
         }
     }
