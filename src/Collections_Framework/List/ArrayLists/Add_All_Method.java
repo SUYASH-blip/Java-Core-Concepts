7ad893af1bd3bdfbefb4cccecdedbe8dd53d347f
suyash giri <suyashgiri938@gmail.com>
2026-07-09 20:22:06 +0530

    Added

--- src/Collections_Framework/List/ArrayLists/Add_All_Method.java
+++ src/Collections_Framework/List/ArrayLists/Add_All_Method.java
@@ -12,7 +12,7 @@ public class Add_All_Method {
         l2.add(6);
         l1.addAll(l2);
         System.out.println(l1);
-        System.out.println(l2);
+      //  System.out.println(l2);
 
 
     }
