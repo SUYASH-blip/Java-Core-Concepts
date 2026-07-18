829ef8b260fdfff09b57d0aaa060d8f3af00031d
suyash giri <suyashgiri938@gmail.com>
2026-07-19 00:37:41 +0530

    Added

--- src/Collections_Framework/List/Vectors/Basic_Methods.java
+++ src/Collections_Framework/List/Vectors/Basic_Methods.java
@@ -29,7 +29,7 @@ public class Basic_Methods {
         System.out.println(result);
         System.out.println(vector1);
         System.out.println(vector2);
-        System.out.println(vector3);
+       // System.out.println(vector3);
 
 
 /*vector 3 ke constructor me hamne pehle vector2 ko likha jab usme kuch bhi
